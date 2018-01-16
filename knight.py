815f4e54c0ce0be007609f901e8c491d4b839a8f
Gregory Kuhn <gregory.kuhn@bd.com>
2018-01-16 16:45:45 +0000

    Adding simple case optimisation

--- knight.py
+++ knight.py
@@ -1,9 +1,11 @@
 import logging
 logger = logging.getLogger()
+
 class Searcher:
     def __init__(self,a=0,b=0,move_count=0):
         self.coord = (a,b) if a<b else (b,a)
         self.move_count = move_count
+        logger.debug("new coord: %s, move_count: %d", str(self.coord), move_count)
 
     def search(self):
         coord = self.coord
@@ -13,14 +15,18 @@ class Searcher:
         max_moves = self.max_moves
         for o in coord:
             if not 0 <= o < self.n:
+                logger.debug("Coord: %s is out of bounds", str(coord))
                 return -1
         other = self.move_history.get(coord, -1)
         if 0 < other < move_count:
+            logger.debug("Got to coord: %s in %d but it has already been reached in %d so killing this path", str(coord), move_count, other)
             return -1
         self.move_history[coord] = move_count
         if move_count >= max_moves:
+            logger.debug("Exceeded max move count for coord: %s", str(coord))
             return -1
         if (a,b) == (self.n-1,self.n-1):
+            logger.debug("Found end in %d moves", move_count)
             return move_count
 
         new_move_count = move_count + 1
@@ -63,7 +69,7 @@ class Knight:
         param = self.param
         other = self.call_history.get(param)
         if other is not None:
-            print(f"Current param: {param}, getting existing result: {other}")
+            logger.debug("Current param: %s, getting existing result:%d ", str(param), other)
             return other
         else:
             searcher = searcher_factory(self.n,param)()
@@ -73,7 +79,7 @@ class Knight:
 
 def knight_factory(n):
     K = type('KnightL', (Knight,), {})
-    K.call_history = {}
+    K.call_history = {(1,1):n-1} #simple case
     K.n = n
     K.max_moves = n*n-1
     return K
