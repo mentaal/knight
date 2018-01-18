071db9dd2584e8730bd00345a2d98f25db452a6a
Gregory Kuhn <gregorykuhn@gmail.com>
2018-01-18 22:53:03 +0000

    continuous on iterative approach

--- conftest.py
+++ conftest.py
@@ -1,3 +1,13 @@
+import pytest
 def pytest_addoption(parser):
     parser.addoption("--board_size", type=int, default=5, help="The size of the board")
     parser.addoption("--param", type=int, nargs=2, help="The movement size")
+
+@pytest.fixture(scope='session')
+def board_size(request):
+    return request.config.getoption("--board_size")
+
+@pytest.fixture(scope='session')
+def param(request):
+    return request.config.getoption("--param")
+
--- knight3.py
+++ knight3.py
@@ -2,24 +2,23 @@ import logging
 logger = logging.getLogger()
 class Node:
 
-    def __init__(self, a=0,b=0,previous=None,move_count=0):
-        self.coord = (a,b) if a<b else (b,a)
+    def __init__(self, coord=(0,0), previous=None,move_count=0):
+        self.coord = coord
         self.previous = previous
         self.move_count = move_count
 
     def __repr__(self):
-        return "Node(a={},b={},previous={},move_count=)".format(
-                *self.coord,
-                self.previous,
+        coord = self.previous.coord if self.previous is not None else 'None'
+        return "Node(coord={},previous={},move_count={})".format(
+                self.coord,
+                coord,
                 self.move_count)
-
 
 class Crawler:
     move_history = {}
 
-    def __init__(self, x,y,n):
-        if x>y:
-            x,y = y,x
+    def __init__(self, param ,n):
+        x,y = param
         self.moves = (
                 (x, +y),
                 (y, +x),
@@ -29,29 +28,37 @@ class Crawler:
                 (y, +x),
                 (x, -y),
                 (y, -x))
-        self.param = x,y
+        self.param = param
+        logger.debug("Using crawler with n set to: %d", n)
         self.n = n
         self.end = (n-1,n-1)
-        self.best = -1
+        self.max_moves = n*n
+        self.best = self.max_moves
 
 
     def next_move(self, node):
         "make this a generator"
+        logger.debug("In next move, with %s as node arg...", node)
         a,b = node.coord
         n = self.n
         new_moves_gen = ((a+x, b+y) for (x,y) in self.moves)
-        new_moves_filtered = ((a,b) if a<b else (b,a)
-                                if (0 <= a < n) and (0 <= b < n))
-        new_move_count = self.move_count + 1
+        new_moves_filtered = set((a,b) if a<b else (b,a) for (a,b) in new_moves_gen
+                                    if (0 <= a < n) and (0 <= b < n))
+        logger.debug("New moves: %s", new_moves_filtered)
+
+        new_move_count = node.move_count + 1
 
         for coord in new_moves_filtered:
+            logger.debug("Checking new coord: %s", str(coord))
             old_move = self.move_history.get(coord)
             if old_move is not None:
-                if old_move.move_count < self.move_count:
+                logger.debug("Found old move with move_count: %d. Current move count: %d", old_move.move_count, node.move_count)
+                if old_move.move_count <= new_move_count:
+                    logger.debug("Got new coord but it's already been visited: %s with <= move count so skipping", old_move)
                     continue
-            new_node = Node(self, *coord, node, new_move_count)
+            new_node = Node(coord, node, new_move_count)
             self.move_history[coord] = new_node
-            logger.debug("From %r, returning new node: %r", node, node)
+            logger.debug("returning new node: %r", new_node)
             return new_node
         logger.debug("From %r, can find no more nodes", node)
         return None
@@ -59,21 +66,42 @@ class Crawler:
     def crawl(self):
         n = Node()
         max_moves = self.max_moves
+        go_back = False
 
         while True:
-            new_node = self.next_move(n)
-            if new_node is None:
+            if go_back:
+                go_back = False
                 previous = n.previous
                 if previous is None:
+                    logger.debug("Finished searching. Returning best result: %d", self.best)
                     return self.best # we're done
-                else:
-                    n = previous
+                n = previous
+            elif n.coord == self.end:
+                count = n.move_count
+                if count < self.best:
+                    self.best = count
+                    logger.debug("Found new best in %d moves", count)
+                    go_back = True
+                    continue
+
+            new_node = self.next_move(n)
+            if new_node is None:
+                go_back = True
             else:
                 n = new_node
-            if n.coord == self.end:
-                self.best = new_node.move_count
-                logger.debug("Found end in %d moves", self.best)
 
+def print_results(n):
+    arg_history = {}
+    r = range(1,n)
 
-
+    for i in r:
+        line_results = []
+        for j in r:
+            coord = (i,j) if i<j else (j,i)
+            result = arg_history.get(coord)
+            if result is None:
+                result = Crawler(coord,n).crawl()
+                arg_history[coord] = result
+            line_results.append(result)
+        print(" ".join(str(num) for num in line_results))
 
